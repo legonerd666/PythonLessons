917e7ec765630fed4562ac4e7e102e47de7b51a0
legonerd666 <chipman.liam@gmail.com>
2023-05-07 15:40:25 -0500

    added logic and input tests

---
+++ inputTest.py
@@ -0,0 +1,7 @@
+name = input("What is your name? ")
+
+if name == "Liam" : 
+    print("Welcome, " + name)
+
+else :
+    print("Access Denied")
---
+++ logicTest.py
@@ -0,0 +1,6 @@
+n = 1
+n2 = 1
+yesNo = True
+
+if n === "1" :
+    print("Is Equal")
