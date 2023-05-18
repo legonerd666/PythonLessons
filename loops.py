298269abfa4325c51329645ca4451d31cae6809a
legonerd666 <chipman.liam@gmail.com>
2023-05-18 12:59:12 -0500

    worked on loops and conditionals

---
+++ loops.py
@@ -0,0 +1,12 @@
+test = ["B", "I", "V", "A", "I", "H", "R"]
+
+for item in test:
+    if item == "B":
+        print("B")
+    elif item == "I":
+        print("I")
+    elif item == "V":
+        print("V")
+    else:
+        print("No")
+
